04b7b5fd6b70d3ac26537ae4e4ec4ef0d664519d
mlietti1 <mcf.lietti@outlook.com>
2023-04-13 21:15:20 +0200

    edit e create post

--- src/main/java/org/lessons/java/springilmiofotoalbum/controller/PostController.java
+++ src/main/java/org/lessons/java/springilmiofotoalbum/controller/PostController.java
@@ -1,14 +1,20 @@
 package org.lessons.java.springilmiofotoalbum.controller;
 
+import jakarta.validation.Valid;
+import org.lessons.java.springilmiofotoalbum.exception.PostNotFoundException;
+import org.lessons.java.springilmiofotoalbum.model.AlertMessage;
+import org.lessons.java.springilmiofotoalbum.model.AlertMessage.AlertMessageType;
 import org.lessons.java.springilmiofotoalbum.model.Post;
 import org.lessons.java.springilmiofotoalbum.service.CategoryService;
 import org.lessons.java.springilmiofotoalbum.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -34,4 +40,73 @@ public class PostController {
         model.addAttribute("list", posts);
         return "posts/index";
     }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable Integer id, Model model){
+        try {
+            Post post = postService.getById(id);
+            model.addAttribute("post", post);
+            return "posts/show";
+        } catch (PostNotFoundException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Post with id " + id + " not found.");
+        }
+    }
+
+    @GetMapping("/create")
+    public String create(Model model){
+        model.addAttribute("post", new Post());
+        model.addAttribute("categoryList", categoryService.getAll());
+        return "posts/create-edit";
+    }
+
+    @PostMapping("/create")
+    public String save(@Valid @ModelAttribute("post") Post formPost, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            model.addAttribute("categoryList", categoryService.getAll());
+            return "posts/create-edit";
+        }
+        postService.createPost(formPost);
+        return "redirect:/posts";
+    }
+
+    @GetMapping("edit/{id}")
+    public String edit(@PathVariable Integer id, Model model){
+        try{
+            Post post = postService.getById(id);
+            model.addAttribute("post", post);
+            model.addAttribute("categoryList", categoryService.getAll());
+            return "posts/create-edit";
+        } catch (PostNotFoundException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Post with id " + id + " not found.");
+        }
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@PathVariable Integer id, @Valid @ModelAttribute("post") Post formPost, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            model.addAttribute("categoryList", categoryService.getAll());
+            return "posts/create-edit";
+        }
+        try{
+            Post updatedPost = postService.updatePost(formPost, id);
+            return "redirect:/posts/" + Integer.toString(updatedPost.getId());
+        } catch (PostNotFoundException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Post with id " + id + " not found.");
+        }
+    }
+
+    public String delete(@PathVariable Integer id, RedirectAttributes redirectAttributes){
+        try{
+            boolean success = postService.deleteById(id);
+            if(success){
+                redirectAttributes.addFlashAttribute("message", new AlertMessage(AlertMessageType.SUCCESS, "Post with id " + id + " deleted successfully." ));
+            } else {
+                redirectAttributes.addFlashAttribute("message", new AlertMessage(AlertMessageType.ERROR, "Unable to delete post with id " + id + "."));
+            }
+        } catch (PostNotFoundException e){
+            redirectAttributes.addAttribute("message", new AlertMessage(AlertMessageType.ERROR, "Post with id " + id + " not found."));
+        }
+        return "redirect:/posts";
+    }
+
 }
