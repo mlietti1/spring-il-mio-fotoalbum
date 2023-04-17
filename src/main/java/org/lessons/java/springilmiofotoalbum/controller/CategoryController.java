7345590fc96339d7c522c1595c9e917be699cd9b
mlietti1 <mcf.lietti@outlook.com>
2023-04-17 12:33:48 +0200

    alert messages and security

--- src/main/java/org/lessons/java/springilmiofotoalbum/controller/CategoryController.java
+++ src/main/java/org/lessons/java/springilmiofotoalbum/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package org.lessons.java.springilmiofotoalbum.controller;
 
 import jakarta.validation.Valid;
+import org.lessons.java.springilmiofotoalbum.model.AlertMessage;
 import org.lessons.java.springilmiofotoalbum.model.Category;
 import org.lessons.java.springilmiofotoalbum.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
 
@@ -30,23 +32,27 @@ public class CategoryController {
     }
 
     @PostMapping("save")
-    public String doSave(@Valid @ModelAttribute(name = "categoryObj") Category category, BindingResult bindingResult, Model model){
+    public String doSave(@Valid @ModelAttribute(name = "categoryObj") Category category, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes){
         if(bindingResult.hasErrors()){
             model.addAttribute("list", categoryService.getAll());
             return "categories/index";
         }
         if(category.getId() != null){
             categoryService.update(category);
+            redirectAttributes.addFlashAttribute("message", new AlertMessage(AlertMessage.AlertMessageType.SUCCESS, "Category updated successfully."));
         } else {
             categoryService.create(category);
+            redirectAttributes.addFlashAttribute("message", new AlertMessage(AlertMessage.AlertMessageType.SUCCESS, "Category created successfully."));
+
         }
         categoryService.create(category);
         return "redirect:/posts/categories";
     }
 
     @GetMapping("delete/{id}")
-    public String delete(@PathVariable Integer id){
+    public String delete(@PathVariable Integer id, RedirectAttributes redirectAttributes){
         categoryService.deleteById(id);
+        redirectAttributes.addFlashAttribute("message", new AlertMessage(AlertMessage.AlertMessageType.SUCCESS, "Category deleted successfully."));
         return "redirect:/posts/categories";
     }
 }
